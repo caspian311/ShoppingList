a804afdbaa4cf0c8ad6b26497795672088a1f888
Matt Todd <matt.c.todd@gmail.com>
2014-02-01 17:19:13 -0600

    initial commit

---
+++ gen/net/todd/shoppinglist/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.todd.shoppinglist;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/net/todd/shoppinglist/MainActivity.java
@@ -0,0 +1,43 @@
+package net.todd.shoppinglist;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_main);
+
+        ImageButton addItemButton = (ImageButton)findViewById(R.id.addItemButton);
+        addItemButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View button) {
+				EditText newItemText = (EditText)findViewById(R.id.newItemText);
+				createNewItem(newItemText.getText().toString());
+			}
+        });
+    }
+    
+    private void createNewItem(String newItemValue) {
+    	LinearLayout listContents = (LinearLayout)findViewById(R.id.listContents);
+    	
+    	LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	LinearLayout newListItemLayout = (LinearLayout)inflater.inflate(R.layout.item_layout, listContents, true);
+    	
+    	TextView newItemTextView = (TextView)newListItemLayout.findViewById(R.id.new_item);
+    	newItemTextView.setText(newItemValue);
+    	
+    	listContents.addView(newListItemLayout, 0);
+    }
+}
