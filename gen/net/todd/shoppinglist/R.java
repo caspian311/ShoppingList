04d574e805401b8f40c443c415a34cc40aaf5556
Matt Todd <matt.c.todd@gmail.com>
2014-02-02 16:50:27 -0600

    added checkboxes to the items added below

--- gen/net/todd/shoppinglist/R.java
+++ gen/net/todd/shoppinglist/R.java
@@ -22,14 +22,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int add_item=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int remove=0x7f020002;
+        public static final int check=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int remove=0x7f020003;
+        public static final int uncheck=0x7f020004;
     }
     public static final class id {
         public static final int addItemButton=0x7f070000;
+        public static final int check_item=0x7f070005;
         public static final int listContents=0x7f070003;
         public static final int newItemText=0x7f070001;
-        public static final int new_item=0x7f070005;
+        public static final int new_item=0x7f070006;
         public static final int remove_item=0x7f070004;
         public static final int scrollView1=0x7f070002;
     }
@@ -40,6 +43,7 @@ public final class R {
     public static final class string {
         public static final int addItemButtonText=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int check_item_text=0x7f050003;
         public static final int remove_item_text=0x7f050002;
     }
     public static final class style {
