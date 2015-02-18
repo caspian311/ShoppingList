918477bb1deb59a6e70bd4d16dd04518ec4e091d
Matt Todd <matt.c.todd@gmail.com>
2015-02-17 19:33:47 -0600

    added robolectric tests

---
+++ app/src/test/java/net/todd/shoppinglist/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package net.todd.shoppinglist;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SimpleUnitTest {
+    @Test
+    public void checkJUnitWork() {
+        // failing test gives much better feedback
+        // to show that all works correctly ;)
+        assertThat(true, is(false));
+    }
+}
