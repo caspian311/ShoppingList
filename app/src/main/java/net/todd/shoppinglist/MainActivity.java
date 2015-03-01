eaa82d1eae70d85e4b6b1a110f3755f75f2edfb5
Matt Todd <matt.c.todd@gmail.com>
2015-03-01 09:55:32 -0600

    moar tests\!

--- app/src/main/java/net/todd/shoppinglist/MainActivity.java
+++ app/src/main/java/net/todd/shoppinglist/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.AsyncQueryHandler;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,32 +16,65 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends Activity {
     public static final int SHOPPING_LIST_LOADER = 1;
     private static final int NEW_ITEM_REQUEST = Activity.RESULT_FIRST_USER + 1;
 
-    private final Uri uri = Uri.parse("content://net.todd.shoppinglist");
-    private SimpleCursorAdapter adapter;
+    public static final Uri URI = Uri.parse("content://net.todd.shoppinglist");
+    private CursorAdapter adapter;
+    private LoaderManager.LoaderCallbacks<Cursor> cursorLoader;
+    private AsyncQueryHandler insertHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adapter = new SimpleCursorAdapter(this, R.layout.shopping_item, null, new String[]{"name"}, new int[]{R.id.shopping_item_name}, 0);
+        insertHandler = new AsyncQueryHandler(getContentResolver()) {
+            @Override
+            protected void onInsertComplete(int token, Object cookie, Uri uri) {
+                super.onInsertComplete(token, cookie, uri);
+                getLoaderManager().restartLoader(SHOPPING_LIST_LOADER, null, cursorLoader);
+            }
+        };
+
+        cursorLoader = new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override
+            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                return new CursorLoader(MainActivity.this, URI,
+                        null, null, null, null);
+            }
+
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+                adapter.swapCursor(data);
+                findViewById(R.id.no_items_available).setVisibility(adapter.getCount() == 0 ? View.VISIBLE : View.GONE);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+                adapter.swapCursor(null);
+            }
+        };
 
         ListView listView = (ListView) findViewById(R.id.list);
+        adapter = createAdapter(this);
         listView.setAdapter(adapter);
+    }
+
+    protected CursorAdapter createAdapter(Context context) {
+        return new SimpleCursorAdapter(context, R.layout.shopping_item, null, new String[]{"name"}, new int[]{R.id.shopping_item_name}, SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        getLoaderManager().initLoader(SHOPPING_LIST_LOADER, null, this);
+        getLoaderManager().initLoader(SHOPPING_LIST_LOADER, null, cursorLoader);
 
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setOnItemLongClickListener(new android.widget.AdapterView.OnItemLongClickListener() {
@@ -59,9 +93,9 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
                                             protected void onDeleteComplete(int token, Object cookie, int result) {
                                                 super.onDeleteComplete(token, cookie, result);
 
-                                                getLoaderManager().restartLoader(SHOPPING_LIST_LOADER, null, MainActivity.this);
+                                                getLoaderManager().restartLoader(SHOPPING_LIST_LOADER, null, cursorLoader);
                                             }
-                                        }.startDelete(-1, null, uri, "" + id, null);
+                                        }.startDelete(-1, null, URI, "" + id, null);
                                     }
                                 })
                         .setNegativeButton(R.string.cancel_delete_button, null)
@@ -78,7 +112,7 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
             }
         });
 
-        getLoaderManager().restartLoader(SHOPPING_LIST_LOADER, null, this);
+        getLoaderManager().restartLoader(SHOPPING_LIST_LOADER, null, cursorLoader);
     }
 
     @Override
@@ -94,13 +128,7 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
         ContentValues contentValues = new ContentValues();
         contentValues.put("value", newItemName);
 
-        new AsyncQueryHandler(getContentResolver()) {
-            @Override
-            protected void onInsertComplete(int token, Object cookie, Uri uri) {
-                super.onInsertComplete(token, cookie, uri);
-                getLoaderManager().restartLoader(SHOPPING_LIST_LOADER, null, MainActivity.this);
-            }
-        }.startInsert(-1, null, uri, contentValues);
+        insertHandler.startInsert(-1, null, URI, contentValues);
     }
 
     @Override
@@ -112,22 +140,5 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
         findViewById(R.id.add_item_button).setOnClickListener(null);
 
         getLoaderManager().destroyLoader(SHOPPING_LIST_LOADER);
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this, Uri.parse("content://net.todd.shoppinglist"),
-                null, null, null, null);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        adapter.swapCursor(data);
-        findViewById(R.id.no_items_available).setVisibility(adapter.getCount() == 0 ? View.VISIBLE : View.GONE);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        adapter.swapCursor(null);
     }
 }
--- app/src/test/java/net/todd/shoppinglist/MainActivityTest.java
+++ app/src/test/java/net/todd/shoppinglist/MainActivityTest.java
@@ -1,22 +1,42 @@
 package net.todd.shoppinglist;
 
 import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.database.MatrixCursor;
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.Robolectric;
 import org.robolectric.util.ActivityController;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(CustomRobolectricRunner.class)
 public class MainActivityTest {
     @Mock
     private static LoaderManager loaderManager;
+    @Mock
+    private static CursorAdapter adapter;
 
     private ActivityController<TestMainActivity> controller;
     private MainActivity testObject;
@@ -25,7 +45,9 @@ public class MainActivityTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        controller = Robolectric.buildActivity(TestMainActivity.class).create().start();
+        when(adapter.getViewTypeCount()).thenReturn(1);
+
+        controller = Robolectric.buildActivity(TestMainActivity.class).create();
         testObject = controller.get();
     }
 
@@ -34,13 +56,44 @@ public class MainActivityTest {
         reset(loaderManager);
         controller.resume();
 
-        verify(loaderManager).initLoader(MainActivity.SHOPPING_LIST_LOADER, null, testObject);
+        verify(loaderManager).initLoader(eq(MainActivity.SHOPPING_LIST_LOADER), any(Bundle.class), any(LoaderManager.LoaderCallbacks.class));
+    }
+
+    @Test
+    public void loaderShouldPullFromCorrectURI() {
+        reset(loaderManager);
+        controller.resume();
+
+        ArgumentCaptor<LoaderManager.LoaderCallbacks> captor = ArgumentCaptor.forClass(LoaderManager.LoaderCallbacks.class);
+        verify(loaderManager).initLoader(eq(MainActivity.SHOPPING_LIST_LOADER), any(Bundle.class), captor.capture());
+        CursorLoader loader = (CursorLoader)captor.getValue().onCreateLoader(0, null);
+
+        assertEquals(MainActivity.URI, loader.getUri());
+    }
+
+    @Test
+    public void whenLoaderFinishes_swapTheCursorOnTheAdapter() {
+        controller.resume();
+        ListView list = (ListView) testObject.findViewById(R.id.list);
+
+        ArgumentCaptor<LoaderManager.LoaderCallbacks> captor = ArgumentCaptor.forClass(LoaderManager.LoaderCallbacks.class);
+        verify(loaderManager).initLoader(eq(MainActivity.SHOPPING_LIST_LOADER), any(Bundle.class), captor.capture());
+
+        Cursor data = mock(Cursor.class);
+        captor.getValue().onLoadFinished(null, data);
+
+        verify(adapter).swapCursor(data);
     }
 
     private static class TestMainActivity extends MainActivity {
         @Override
         public LoaderManager getLoaderManager() {
             return loaderManager;
+        }
+
+        @Override
+        protected CursorAdapter createAdapter(Context context) {
+            return adapter;
         }
     }
 }
